f6dbd2d83c50bf69453ae9de7e39a73538b12894
Elizeu Santos <elizeusdsantos@gmail.com>
2022-03-17 08:39:36 +0100

    Update Gender.java

--- subscription/src/main/java/com/adidas/challenge/subscription/model/Gender.java
+++ subscription/src/main/java/com/adidas/challenge/subscription/model/Gender.java
@@ -1,4 +1,17 @@
 package com.adidas.challenge.subscription.model;
 
 public enum Gender {
+  MALE("male"),
+  FEMALE("female"),
+  NON_BINARY("other");
+
+  String key;
+
+  Gender(String key) {
+    this.key = key;
+  }
+
+  public String getKey() {
+    return this.key;
+  }
 }
