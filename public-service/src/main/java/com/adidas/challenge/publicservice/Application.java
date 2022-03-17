654a2ae082557cf1bd51d8d9c6e87f50478c5f25
Elizeu Santos <elizeusdsantos@gmail.com>
2022-03-17 10:25:56 +0100

    CLEANUP

--- public-service/src/main/java/com/adidas/challenge/publicservice/Application.java
+++ public-service/src/main/java/com/adidas/challenge/publicservice/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PublicServiceApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PublicServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
 
+  }
 }
