14939233710090c3386432565625540e9571bb1f
Elizeu Santos <elizeusdsantos@gmail.com>
2022-03-17 08:39:22 +0100

    Make columns required

--- subscription/src/main/resources/db/migration/V1__Create_subscription_table.sql
+++ subscription/src/main/resources/db/migration/V1__Create_subscription_table.sql
@@ -1,12 +1,12 @@
 --liquibase formatted sql
 
 CREATE TABLE subscription (
-  id UUID,
+  id UUID NOT NULL,
   subscription_id SERIAL PRIMARY KEY,
-  email VARCHAR(200),
+  email VARCHAR(200) NOT NULL,
   first_name VARCHAR(50),
   gender VARCHAR(15),
-  birth_date DATE,
-  active boolean,
-  campaign_id INT
+  birth_date DATE NOT NULL,
+  active boolean NOT NULL,
+  campaign_id INT NOT NULL
 )
